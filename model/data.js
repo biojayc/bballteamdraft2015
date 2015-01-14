3e6bd5d302776e6a4502a377296cac1a18f239a0
Jamie Smith <biojayc@gmail.com>
2015-01-13 23:58:28 -0800

    Updating datainjector to use game_scraper data.

--- model/data.js
+++ model/data.js
@@ -1,5 +1,4 @@
-var games = require('./games').games,
-    util = require('./util');
+var fs = require('fs');
 
 var owners = [
   { id: 'MM', name: 'Mark Mullings', first: "Mark", initial: "M", img: "images/noimage.gif" },
@@ -44,6 +43,38 @@ var teams = [
   { id: 'BOS', name: 'Boston Celtics', owner: 'BS'  }
 ];
 
+var makeGame = function(cols) {
+  return {
+    date: cols[0],
+    time: cols[1],
+    awayId: cols[2],
+    homeId: cols[3],
+    awayScore: cols[4],
+    homeScore: cols[5],
+  }
+}
+
+//TODO: get rid of the sync methods here.  We shouldn't use any sync methods on the web server.
+var getGames = function() {
+  var games = [];
+  var files = fs.readdirSync('./game_scraper/final/');
+  //get newest file
+  if (files.length > 0) {
+    var file = files[files.length-1];
+    var path = './game_scraper/final/' + file;
+    var data = fs.readFileSync(path, 'utf8');
+    var rows = data.split('\n');
+    for (var i = 0; i < rows.length; i++) {
+      var row = rows[i];
+      var cols = row.split('\t');
+      if (cols.length == 6) {
+        games.push(makeGame(cols));
+      }
+    }
+  }
+  return games;
+}
+
 exports.injectData = function(controller) {
   for(var i = 0; i < owners.length; i++) {
     var owner = owners[i];
@@ -53,8 +84,9 @@ exports.injectData = function(controller) {
     var team = teams[i];
     controller.addTeam(team.id, team.name, team.owner);
   }
+  var games = getGames();
   for(var i = 0; i < games.length; i++) {
     var game = games[i];
-    controller.addGame(game.date, game.time, game.awayId, game.homeId, game.awayScore, game.homeScore);
+    controller.addGame(game.date, game.time, game.awayId, game.homeId, parseInt(game.awayScore), parseInt(game.homeScore));
   }
 }
