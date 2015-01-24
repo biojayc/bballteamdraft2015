f165713f07f5b688033ad9bbd7c819796dbb12cb
Jamie Smith <biojayc@gmail.com>
2015-01-24 13:26:43 -0800

    Added static file filter to logs dashboard.

--- logs_processor/main.js
+++ logs_processor/main.js
@@ -8,24 +8,34 @@ var processed_logs = logs_processor.process_logs('../logs/');
 restify.startWebServer(1338);
 
 restify.registerRoute("/", "GET", function(req, res) {
-  var vars = {
-    requests: processed_logs.requests,
-  };
-
-  new layout.LayoutEngine('index.html',undefined, vars).render(function(html) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-  });
-});
-
-restify.registerRoute("/ip.*", "GET", function(req, res) {
-  var ip;
   var queryObj = requestUtils.getQueryObj(req);
-  if (queryObj && queryObj['ip']) {
-    var ip = queryObj['ip'];
+  if (queryObj) {
+    if (queryObj['filter']) {
+      var filter = queryObj['filter'];
+    }
+    if (queryObj['ip']) {
+      var ip = queryObj['ip'];
+    } 
+  }
+  if (ip) {
+    var requests = processed_logs.ipHash[ip].slice();
+  } else {
+    var requests = processed_logs.requests.slice();
+  }
+  if (filter == "static") {
+    for (var i = 0; i < requests.length;i++) {
+      var request = requests[i];
+      if (request.url.indexOf('/static')== 0 ||
+          request.url.indexOf('/css') == 0 ||
+          request.url.indexOf('/images') == 0 ||
+          request.url.indexOf('/favicon.ico') == 0) {
+        requests.splice(i, 1);
+        i--;
+      }
+    }
   }
   var vars = {
-    requests: processed_logs.ipHash[ip],
+    requests: requests,
   };
 
   new layout.LayoutEngine('index.html',undefined, vars).render(function(html) {
