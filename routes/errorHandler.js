40ac3d96dcd2a88cce3544fe6cf25d0626cb95aa
Jamie Smith <biojayc@gmail.com>
2016-10-16 22:30:06 -0700

    fixing the error handler

--- routes/errorHandler.js
+++ routes/errorHandler.js
@@ -2,7 +2,7 @@ var layout = require('../layout'),
     url = require('url');
 
 var error = function(req, res) {
-  layout.LayoutEngine.create("layouts/error.html").render(function(html) {
+  layout.create("layouts/error.html").render(function(html) {
     res.writeHead(404, {'Content-Type': 'text/html'});
     res.end(html);	
   });
